163ea350d8681c02c6da7cbd72a99744864d2aae
ZJU_czx <952370295@qq.com>
2026-03-02 11:46:05 +0800

    fix: set titleOnDeimensions based on indicators also

--- packages/vseed/src/pipeline/spec/table/pipes/corner/titleOnDimension.ts
+++ packages/vseed/src/pipeline/spec/table/pipes/corner/titleOnDimension.ts
@@ -1,10 +1,13 @@
 import type { PivotTableSpecPipe } from 'src/types'
 
 export const titleOnDimension: PivotTableSpecPipe = (spec) => {
+  const { rows = [], columns = [], indicatorsAsCol, indicators = [] } = spec
+  const rowsLength = rows.length + (indicatorsAsCol ? 0 : indicators?.length > 0 ? 1 : 0)
+
   return {
     ...spec,
     corner: {
-      titleOnDimension: 'row',
+      titleOnDimension: rowsLength <= 1 && columns.length >= 1 ? 'column' : 'row',
     },
   }
 }
