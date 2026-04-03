8cf7662519875f8ac4391f348a078d8a1a8511b0
jiangtao.young <jiangtao.young@qq.com>
2026-04-03 14:38:44 +0800

    test(vbi): restore relative date snapshot

--- packages/vbi/tests/examples/whereFilter/whereFilter.test.ts
+++ packages/vbi/tests/examples/whereFilter/whereFilter.test.ts
@@ -1997,12 +1997,12 @@ describe('WhereFilter', () => {
             {
               "field": "order_date",
               "op": ">=",
-              "value": "2026-02-22",
+              "value": "2026-02-21",
             },
             {
               "field": "order_date",
               "op": "<",
-              "value": "2026-03-24",
+              "value": "2026-03-23",
             },
             {
               "field": "sales",
